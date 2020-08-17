b00b077ecbb5e67db5e0339c6d4ce97f6c260f57
dawood96 <46974692+dawood96@users.noreply.github.com>
2020-08-17 17:42:02 -0400

    Updates

---
+++ precache-manifest.1e20b0ed784608bc2cf40e2045ba181e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d54e23046949eb4309ee2dbb787839b",
+    "url": "/davidshammas/index.html"
+  },
+  {
+    "revision": "bd770cc3f17659570400",
+    "url": "/davidshammas/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "98fd278e856a3e654a4d",
+    "url": "/davidshammas/static/css/main.121e32fa.chunk.css"
+  },
+  {
+    "revision": "bd770cc3f17659570400",
+    "url": "/davidshammas/static/js/2.149beaeb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/davidshammas/static/js/2.149beaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98fd278e856a3e654a4d",
+    "url": "/davidshammas/static/js/main.aa9160c2.chunk.js"
+  },
+  {
+    "revision": "8d3a240d0920ec7b1b0c",
+    "url": "/davidshammas/static/js/runtime-main.59819905.js"
+  },
+  {
+    "revision": "2793ba24d3641abda0ef324dc2f43583",
+    "url": "/davidshammas/static/media/Captureicecream.2793ba24.PNG"
+  },
+  {
+    "revision": "f92eba889664716a1e92449c8e2bd337",
+    "url": "/davidshammas/static/media/bbk.f92eba88.PNG"
+  },
+  {
+    "revision": "c20aba2bd70bba3eb2341122c0701fc0",
+    "url": "/davidshammas/static/media/bg.c20aba2b.png"
+  },
+  {
+    "revision": "12ac9d98ce6ba5dd1a286cbfa3cd52e7",
+    "url": "/davidshammas/static/media/blog-img.12ac9d98.jpeg"
+  },
+  {
+    "revision": "b4b230a2085312ac82d71e4874b00296",
+    "url": "/davidshammas/static/media/booklist.b4b230a2.png"
+  },
+  {
+    "revision": "0f4596f4b6aa50fdb83b31e24bb98296",
+    "url": "/davidshammas/static/media/budgetbookkeeper.0f4596f4.PNG"
+  },
+  {
+    "revision": "f1b8d0259e30bf23a6644dd956bf5fda",
+    "url": "/davidshammas/static/media/contact.f1b8d025.jpg"
+  },
+  {
+    "revision": "54ced7998925b36ccf4c9a27ca20043b",
+    "url": "/davidshammas/static/media/david-shammas.54ced799.jpg"
+  },
+  {
+    "revision": "c9a85343472040c81cde38e1653d199b",
+    "url": "/davidshammas/static/media/exchange.c9a85343.png"
+  },
+  {
+    "revision": "f733ef841fc7ddd330dc1b6d723dc767",
+    "url": "/davidshammas/static/media/fr.f733ef84.png"
+  },
+  {
+    "revision": "b53b611f31a2a989f3fee931f0dc75d1",
+    "url": "/davidshammas/static/media/md.b53b611f.png"
+  },
+  {
+    "revision": "2274171068a4bce13a28f009276df7b8",
+    "url": "/davidshammas/static/media/portfolio.22741710.png"
+  },
+  {
+    "revision": "d95ac601ef1f5ba1f34a5fd44e3d7a3c",
+    "url": "/davidshammas/static/media/rf.d95ac601.png"
+  },
+  {
+    "revision": "807598925773fdc976bc9cac6d3457fa",
+    "url": "/davidshammas/static/media/shoppingList.80759892.png"
+  },
+  {
+    "revision": "9de49133c1a14b8d527660070ec82dad",
+    "url": "/davidshammas/static/media/skill-bg.9de49133.jpeg"
+  },
+  {
+    "revision": "881936435b5131b932a9ef263a73b086",
+    "url": "/davidshammas/static/media/todolist.88193643.png"
+  }
+]);
